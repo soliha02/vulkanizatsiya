b662b0d2b33286f788759ea5ebc2ca61ad94d537
soliha02 <164037162+soliha02@users.noreply.github.com>
2024-06-18 20:19:41 +0500

    Add files via upload

---
+++ negr.js
@@ -0,0 +1,14 @@
+let son = prompt("son kiriting")
+let son2 = prompt("ikkinchi sonni kiriting")
+
+
+function Kattason(a, b) {
+    if (a > b) {
+        alert(a + " > " + b)
+    }else if (b > a) {
+        alert(b + " > " + a)
+    }else if (b = a) {
+        alert(b + " = " + a)
+    }
+}
+Kattason(son, son2)
